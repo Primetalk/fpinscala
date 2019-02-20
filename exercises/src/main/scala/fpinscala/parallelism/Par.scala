d6346c9804f89d2f5381afc3ff8348f70d5312d3
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2019-02-20 22:43:58 +0300

    Implement Map2Future and a few helper methods

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -1,6 +1,8 @@
 package fpinscala.parallelism
 
 import java.util.concurrent._
+import java.util.concurrent.atomic.AtomicReference
+
 import language.implicitConversions
 
 object Par {
@@ -12,16 +14,37 @@ object Par {
   
   private case class UnitFuture[A](get: A) extends Future[A] {
     def isDone = true 
-    def get(timeout: Long, units: TimeUnit) = get 
+    def get(timeout: Long, units: TimeUnit) = get
     def isCancelled = false 
     def cancel(evenIfRunning: Boolean): Boolean = false 
   }
-  
+  /** @param f is fast. */
+  private case class Map2Future[A, B, C](a: Future[A], b: Future[B], f: (A,B) => C) extends Future[C] {
+    def isDone: Boolean = a.isDone && b.isDone
+
+    private val result: AtomicReference[Option[C]] = new AtomicReference(None)
+
+    def get: C = result.get().getOrElse{
+      val res = f(a.get, b.get)
+      result.compareAndSet(None, Some(res))
+      res
+    }
+    // // This implementation of `map2` does _not_ respect timeouts, and eagerly waits for the returned futures. This means that even if you have passed in "forked" arguments, using this map2 on them will make them wait. It simply passes the `ExecutorService` on to both `Par` values, waits for the results of the Futures `af` and `bf`, applies `f` to them, and wraps them in a `UnitFuture`. In order to respect timeouts, we'd need a new `Future` implementation that records the amount of time spent evaluating `af`, then subtracts that time from the available time allocated for evaluating `bf`.
+    def get(timeout: Long, units: TimeUnit): C =
+      result.get().getOrElse{
+        val res = f(a.get(timeout, units), b.get(timeout, units))
+        result.compareAndSet(None, Some(res))
+        res
+      }
+    def isCancelled: Boolean = a.isCancelled || b.isCancelled
+    def cancel(evenIfRunning: Boolean): Boolean = a.cancel(evenIfRunning) || b.cancel(evenIfRunning)
+  }
+
   def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = // `map2` doesn't evaluate the call to `f` in a separate logical thread, in accord with our design choice of having `fork` be the sole function in the API for controlling parallelism. We can always do `fork(map2(a,b)(f))` if we want the evaluation of `f` to occur in a separate thread.
     (es: ExecutorService) => {
       val af = a(es) 
       val bf = b(es)
-      UnitFuture(f(af.get, bf.get)) // This implementation of `map2` does _not_ respect timeouts, and eagerly waits for the returned futures. This means that even if you have passed in "forked" arguments, using this map2 on them will make them wait. It simply passes the `ExecutorService` on to both `Par` values, waits for the results of the Futures `af` and `bf`, applies `f` to them, and wraps them in a `UnitFuture`. In order to respect timeouts, we'd need a new `Future` implementation that records the amount of time spent evaluating `af`, then subtracts that time from the available time allocated for evaluating `bf`.
+      Map2Future(af, bf, f)
     }
   
   def fork[A](a: => Par[A]): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
@@ -29,10 +52,13 @@ object Par {
       def call = a(es).get
     })
 
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
   def map[A,B](pa: Par[A])(f: A => B): Par[B] = 
     map2(pa, unit(()))((a,_) => f(a))
 
-  def sortPar(parList: Par[List[Int]]) = map(parList)(_.sorted)
+  def sortPar(parList: Par[List[Int]]) =
+    map(parList)(_.sorted)
 
   def equal[A](e: ExecutorService)(p: Par[A], p2: Par[A]): Boolean = 
     p(e).get == p2(e).get
@@ -49,9 +75,29 @@ object Par {
   implicit def toParOps[A](p: Par[A]): ParOps[A] = new ParOps(p)
 
   class ParOps[A](p: Par[A]) {
+    def map[B](f: A => B): Par[B] =
+      Par.map(p)(f)
 
-
+    def flatMap[B](f: A => Par[B]): Par[B] = es => {
+      val fab: Future[Par[B]] = map(f)(es)
+      es.submit[B](() => fab.get.apply(es).get)
+    }
   }
+
+  def asyncF[A,B](f: A => B): A => Par[B] =
+    a => lazyUnit(f(a))
+
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] = ps match {
+    case Nil => unit(Nil)
+    case h :: t => map2(h, sequence(t))((fh,tt) => fh :: tt)
+  }
+
+  def parMap[A,B](ps: List[A])(f: A => B): Par[List[B]] =
+    fork(sequence(ps.map(asyncF(f))))
+
+  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] =
+    fork(parMap[A, Option[A]](as)(a => Some(a).filter(f)).map(_.flatten))
+
 }
 
 object Examples {
@@ -65,3 +111,26 @@ object Examples {
     }
 
 }
+/*
+map(y)(id) == y,
+it’s a free theorem that
+map(map(y)(g))(f) == map(y)(f compose g)
+
+1. let's replace y with map(z)(g):
+
+map(  map(z)(g)  )(id) == map(z)(g)
+
+2. let z == unit(a)
+
+map(unit(a))(g) == unit(g(a))
+
+map(  map(unit(a))(g)  )(id) == map(unit(a))(g)
+map(  unit(g(a))  )(id) == unit(g(a))
+
+3. Let's replace id with a function f
+
+map(  unit(g(a))  )(f) == unit(f(g(a)))
+
+
+fork(fork(fork(...))) - will deadlock on a thread pool of size n
+ */
